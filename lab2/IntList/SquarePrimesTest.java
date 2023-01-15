e98a9e23a1ef8e561ede7ec17cab3c2090edca5c
18230147540 <2822504207@qq.com>
2023-01-15 21:05:18 +0800

    the last change include  testQequarePrimeSimple

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,11 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+    @Test
+    public void tsetSquarePrimesSimple(){
+        IntList  lst =IntList.of(1,2,3,4,5,6,7,8,9,0,23,2);
+        boolean changed=IntListExercises.squarePrimes(lst);
+        assertEquals("1 -> 4 -> 9 -> 4 -> 25 -> 6 -> 49 -> 8 -> 9 -> 0 -> 529 -> 4",lst.toString());
+        assertTrue(changed);
+    }
 }
